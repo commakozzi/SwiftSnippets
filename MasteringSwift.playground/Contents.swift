402ca910999b7cf84bb2df79758559eba6a6298b
Commakozzi <commakozzi@Commakozzis-Mac.local>
2018-03-13 12:33:30 -0700

    Update

---
+++ MasteringSwift.playground/Contents.swift
@@ -0,0 +1,74 @@
+protocol Bird {
+    var name: String { get }
+    var canFly: Bool { get }
+}
+
+extension Bird {
+    // Flyable birds can fly!
+    var canFly: Bool { return self is Flyable }
+}
+
+protocol Flyable {
+    var airspeedVelocity: Double { get }
+}
+
+struct FlappyBird: Bird, Flyable {
+    let name: String
+    let flappyAmplitude: Double
+    let flappyFrequency: Double
+    var airspeedVelocity: Double {
+        return 3 * flappyFrequency * flappyAmplitude
+    }
+}
+
+struct Penguin: Bird {
+    let name: String
+    let canFly = false
+}
+
+struct SwiftBird: Bird, Flyable {
+    var name: String { return "Swift \(version)"}
+    let version: Double
+    
+    // Swift is FASTER every version!
+    var airspeedVelocity: Double { return version * 1000.0 }
+}
+
+enum UnladenSwallow: Bird, Flyable {
+    case african
+    case european
+    case unknown
+    
+    var name: String {
+        switch self {
+        case .african:
+            return "African"
+        case .european:
+            return "European"
+        case .unknown:
+            return "What do you mean? African or European"
+        }
+    }
+    
+    var airspeedVelocity: Double {
+        switch self {
+        case .african:
+            return 10.0
+        case .european:
+            return 9.9
+        case .unknown:
+            fatalError("You are thrown from the bridge of death!")
+        }
+    }
+}
+
+extension UnladenSwallow {
+    var canFly: Bool {
+        return self != .unknown
+    }
+}
+
+UnladenSwallow.unknown.canFly
+UnladenSwallow.african.canFly
+UnladenSwallow.european.canFly
+Penguin(name: "King Penguin").canFly
